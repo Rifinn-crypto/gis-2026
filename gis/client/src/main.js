63d776df932aaede22c8a78d91c204913fa6b06c
Rifinn-crypto <114596909+Rifinn-crypto@users.noreply.github.com>
2026-04-11 12:11:03 +0400

    Инициализация слоев в одном месте

--- gis/client/src/main.js
+++ gis/client/src/main.js
@@ -6,32 +6,36 @@ import ImageLayer from 'ol/layer/Image';
 import ImageWMS from 'ol/source/ImageWMS';
 import { fromLonLat } from 'ol/proj';
 
+const createWMSLayer = (layerName) => {
+  return new ImageLayer({
+    source: new ImageWMS({
+      url: "http://localhost:8080/geoserver/gis/wms",
+      params: {
+        LAYERS: `gis:${layerName}`,
+        TILED: true,
+        TRANSPARENT: true,
+        VERSION: "1.1.1"
+      },
+      ratio: 1,
+      serverType: "geoserver",
+    }),
+  });
+};
+
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new OSM()
-    })
+    }),
+    createWMSLayer('buildings'),
+    createWMSLayer('roads'),
+    createWMSLayer('poi')
   ],
   view: new View({
-
     center: fromLonLat([50.2467455, 53.4376983]),
     zoom: 16
   })
 });
 
-const buildingsLayer = new ImageLayer({
-  source: new ImageWMS({
-    url: "http://localhost:8080/geoserver/gis/wms",
-    params: {
-      LAYERS: "gis:buildings",
-      TILED: true,
-      TRANSPARENT: true,
-      VERSION: "1.1.1"
-    },
-    ratio: 1,
-    serverType: "geoserver",
-  }),
-});
-
-map.addLayer(buildingsLayer);
+console.log('Карта готова. Слои: buildings, roads, poi');
